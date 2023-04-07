eeb771bbd215c3f48d47b53fafff5560f71f59c1
Dmitriy Fourier <dmitriyfurye@gmail.com>
2023-04-07 22:40:13 +0500

    ad tg comments color changer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  
+  colors: {
+    'tg-dark': '#15202b',
+    'tg-light' '#fff',
+
+  },
   darkMode: 'class',
   content: ['./src/**/*.md', './src/**/*.html', './src/_includes/**/*.liquid', './node_modules/flowbite/**/*.js'],
   plugins: [
