521500abe1993e12174fd30d79a697e8c7aa23b8
Dmitriy Fourier <dmitriyfurye@gmail.com>
2023-04-09 23:51:45 +0500

    Fix color links

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,19 @@ module.exports = {
         background: '#2e2e2e',
         comments: '#797979',
         white: '#d6d6d6',
-        yellow: '#e5b567',
+        yellow: {
+                  main: 'rgb(229,181,103)',
+                  deco: 'rgb(229,181,103, 0.25)',
+                },
         green: '#b4d273',
-        orange: '#e87d3e',
-        purple: '#9e86c8',
+        orange: {
+                  main: 'rgb(232,125,62)',
+                  deco: 'rgb(232,125,62, 0.25)',
+                },
+        purple: {
+                  main: 'rgb(158,134,200)',
+                  deco: 'rgb(158,134,200, 0.25)',
+                },
         pink: '#b05279',
         blue: '#6c99bb',
       },
